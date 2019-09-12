7c775555f46cf098bea4c5d8e13f602ff5c21a98
mayrton <ayrtonmatthew@yahoo.com>
2019-09-12 14:24:57 -0600

    copied images and app.js to new folder

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -21,9 +21,156 @@ const quizQuestions = [
 
     {
         question: "What was the name of the first satelite in space?",
-        choices: ["Sputnik", "exploer 1", "Voyager", "Skylab"],
+        choices: ["Sputnik", "Explorer 1", "Voyager", "Skylab"],
         correctAnswer: "Sputnik"
 
     },
 
 ];
+
+const funImages =[
+    '../assets/images/moon landing.gif',
+    '../assets/images/planets.gif',
+    '../assets/images/einstein clapping.gif',
+    '../assets/images/sputnik.gif'
+];
+
+const sadImages =[
+    
+    '../assets/images/rocket explosion.gif',
+    '../assets/images/planet exploding.gif',
+    '../assets/images/rick crying.gif',
+    '../assets/images/satellite explosion.gif'
+];
+
+//Initial Values 
+var counter = 5;
+var currentQuestion = 0;
+var score = 0;
+var lost = 0;
+var timer;
+
+//If the Time is over go to the next question.
+function nextQuestion() {
+
+    const isQuestionOver=(quizQuestions.length -1)===currentQuestion;
+    if (isQuestionOver) {
+        console.log('Game is Over');
+        displayResult();
+    } else {
+
+        currentQuestion++;
+        loadQuestion();
+    }
+
+}
+// Start a time of 30 seconds for the player to choose an answer from each question. 
+function timeUp() {
+    clearInterval(timer);
+
+    lost++;
+
+    preloadImage('lost');
+    setTimeout(nextQuestion, 3 * 1000);
+    // nextQuestion();
+}
+
+function CountDown() {
+    counter--;
+
+    $('#time').html('Timer:' + counter);
+
+    if (counter === 0) {
+        timeUp();
+    }
+}
+// Displaying the questions and choices in the browser screen.
+
+function loadQuestion() {
+    counter = 5;
+    timer = setInterval(CountDown, 1000);
+
+    const question = quizQuestions[currentQuestion].question;
+    const choices = quizQuestions[currentQuestion].choices;
+
+    $('#time').html("Timer:" + counter);
+    $("#game").html(`<h4> ${question} </h4>${loadChoices(choices)}`);
+
+}
+
+function loadChoices(choices) {
+    var result = "";
+
+    for (var i = 0; i < choices.length; i++) {
+        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+    }
+
+    return result;
+}
+
+// If a right or wrong choice is selected go to the next question.
+$(document).on('click', ".choice", function(){
+        clearInterval(timer);
+        const selectedAnswer = $(this).attr('data-answer');
+        const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+        if (correctAnswer===selectedAnswer){
+            //User Wins
+            score++;
+            setTimeout(nextQuestion, 3 * 1000);
+            console.log("Wins");
+            preloadImage('win');
+        } else{
+            lost++;
+            setTimeout(nextQuestion, 3 * 1000);
+            preloadImage('lost');
+            console.log("lost!");
+        }
+        
+});
+
+function displayResult() {
+    const result=`
+    <p>You get ${score} questions(s) right </p>
+    <p>You got it wrong ${lost} questions(s) </p>
+    <p>Total questions ${quizQuestions.length} questions(s) </p>
+    <button class="btn btn-primary" id="reset" >Reset Game </button>
+    `;
+    $("#game").html(result);
+}
+
+$(document).on('click', '#reset', function(){
+counter = 5;
+currentQuestion = 0;
+score = 0;
+lost = 0;
+timer=null;
+
+   loadQuestion();
+});
+
+function loadRemainingQuestion(){
+    const remainingQuestion = quizQuestions.length-(currentQuestion + 1)
+    const TotalQuestion = quizQuestions.length;
+    
+    return `Remaining Question:${requestAnimationFrame}/${totalQuestion}`;
+}
+// Show funny gif pictures if correct or incorrect answers. 
+function preloadImage(status){
+    const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+    if (status==='win'){
+        $('#game').html(`
+            <p class='preload-image'> You Picked a Winner that's out of this World!</p>
+            <p class='preload-image'> The correct answer is ${correctAnswer} </p>
+        `);
+    } else{
+        $('#game').html(`
+        <p class='preload-image'> Noooo! I'm sorry the correct answer was ${correctAnswer}</p>
+        <p class='preload-image'> You really crashed and burned!!  </p>
+    `);
+    }
+
+}
+
+// loadimage ('');
+loadQuestion();
